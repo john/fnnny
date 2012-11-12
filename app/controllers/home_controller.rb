a3d6ce4c20ea39c1e52abedf649d384dbddc3c28
John McGrath <john@entelo.com>
2012-11-12 12:57:03 -0800

    show follower's items on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,23 @@
 class HomeController < ApplicationController
 
   def index
-    @title = "#{configatron.app_name}: Delight and amuse"
+    @title = "#{configatron.app_name}: To delight and amuse"
     
-    # all items
-    @items = Item.find(:all, :order => 'created_at DESC')
-    
-    # items from followers
-    # @items = ...
+    if signed_in?
+      if current_user.following_by_type_count('User') >= 3
+        
+        # get all items created by the people you're following
+        # adapted from: http://stackoverflow.com/questions/7920082/get-posts-of-followed-users-with-acts-as-follower
+        @items = User.find(current_user.id).following_users.includes(:items).collect{|u| u.items}.flatten
+        
+      else
+        
+        # add a dismissible banner pushing users to follow people to see scoped results.
+        
+        # all items
+        @items = Item.find(:all, :order => 'created_at DESC').limit(20)
+      end
+    end
     
   end
   
