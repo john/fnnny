7325a095516f98bb9176b9d4cba25e9ac5a1ed2f
John McGrath <john@entelo.com>
2013-01-20 17:08:47 -0800

    added jruby-json gem

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ require 'bundler/capistrano'
 #   end
 # end
 
-server "ec2-23-20-55-66.compute-1.amazonaws.com", :app, :primary => true
+server "ec2-23-23-46-250.compute-1.amazonaws.com", :app, :primary => true
 
 ssh_options[:port] = 22
 ssh_options[:keys] = "~/.ssh/id_rsa"
