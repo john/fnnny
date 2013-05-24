e6dd62a4851e385c4a8317a448fab4edb4df7465
John McGrath <john@entelo.com>
2013-05-23 21:50:36 -0700

    add helpers to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery
   
+  helper :all
+  
   def after_sign_in_path_for(resource)
     # request.env['omniauth.origin'] || stored_location_for(resource) || root_path
     friends_person_path(resource)
